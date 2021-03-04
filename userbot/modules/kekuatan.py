5b9a5e782a4093ff0aa1a0192ff93727b8b2bbf9
vivianokrzz <75564456+vivianokrzz@users.noreply.github.com>
2021-03-04 13:38:12 +0700

    Update kekuatan.py

--- userbot/modules/kekuatan.py
+++ userbot/modules/kekuatan.py
@@ -7,19 +7,19 @@ from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
 
 @register(outgoing=True, pattern=r'^.kekuatan(:? |$)([1-8])?')
 async def _(fry):
-    await fry.edit("`Lord Mengaktifkan Kekuatan Telegram...👾`")
+    await fry.edit("`Mampos Raimu peyot😈`")
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
         return
     if not fry.reply_to_msg_id:
-        await fry.edit("`Mohon Balas Di Sticker Lord`")
+        await fry.edit("`Bocah tolol bales di stiker asu!!`")
         return
     reply_message = await fry.get_reply_message()
     if not reply_message.media:
-        await fry.edit("`Gambar tidak di dukung`")
+        await fry.edit("`Ohh ora iso iki!!`")
         return
     if reply_message.sender.bot:
-        await fry.edit("`Mohon Balas Di Sticker Lord`")
+        await fry.edit("`Bocah tolol bales di stiker asu`")
         return
     chat = "@image_deepfrybot"
     message_id_to_reply = fry.message.reply_to_msg_id
